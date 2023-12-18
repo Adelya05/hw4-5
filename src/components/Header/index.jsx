cc3237b9a7eed0c95366276480b004e77d9335b6
adelya <aki2015112@gmail.com>
2023-12-18 18:07:54 +0600

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../Header/header.scss'
+import user from '../../assets/user.png';
+import { Link } from 'react-router-dom';
+ 
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <Link to="/">
+        <div>Movie App</div>
+      </Link>
+ 
+    </div>
+  )
+}
+
+export default Header;
